0a7bb64d4e0c57d24ab6ee9675b79d6125793f51
chenard612 <chenard612@hotmail.com>
2025-06-08 01:15:54 -0400

    we need 3 days as per the requirements

--- app/api.py
+++ app/api.py
@@ -7,33 +7,34 @@ from app.parser import parse_csv_file
 BASE_URL = "https://twtransfer.energytransfer.com/ipost/TW/capacity/operationally-available"
 
 def query_csv():
-    response = requests.get(BASE_URL)
+    for delta in range(3):
+        date = datetime.utcnow() - timedelta(days=delta)
+        response = requests.get(BASE_URL)
 
-    if response.status_code != 200:
-        print(f"[{date.date()}] Failed to load HTML page")
-        return
+        if response.status_code != 200:
+            print(f"[{date.date()}] Failed to load HTML page")
+            continue
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    menu_spans = soup.find_all("span", class_="menuButton")
+        soup = BeautifulSoup(response.text, "html.parser")
+        menu_spans = soup.find_all("span", class_="menuButton")
 
-    csv_url = None
-    for span in menu_spans:
-        link = span.find("a", class_="csv")
-        if link and "csv" in link["href"]:
-            relative_csv_link = link["href"]
-            csv_url = urljoin(BASE_URL, relative_csv_link)
-            break
+        csv_url = None
+        for span in menu_spans:
+            link = span.find("a", class_="csv")
+            if link and "csv" in link["href"]:
+                relative_csv_link = link["href"]
+                csv_url = urljoin(BASE_URL, relative_csv_link)
+                break
 
-    if not csv_url:
-        print(f"CSV link not found")
-        return
+        if not csv_url:
+            print(f"[{date.date()}] CSV link not found")
+            continue
 
-    csv_response = requests.get(csv_url)
+        csv_response = requests.get(csv_url)
 
 
-    if csv_response.status_code == 200:
-        print(f"[Downloaded CSV successfully]")
-        parse_csv_file(csv_response.text)
-    else:
-        print(f"Failed to download for {date.date()}")
-        return
+        if csv_response.status_code == 200:
+            print(f"[{date.date()}] Downloaded CSV successfully")
+            parse_csv_file(csv_response.text)
+        else:
+            print(f"Failed to download for {date.date()}")
